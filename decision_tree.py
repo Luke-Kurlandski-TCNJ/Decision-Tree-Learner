55707c139c6186348b0c5fb592f7d54dd9a9af90
Luke Kurlandski <kurlanl1@dev001.cm.cluster>
2021-04-11 16:25:11 -0400

    Added pseudocode and scaffolding for the project

---
+++ decision_tree.py
@@ -0,0 +1,136 @@
+"""
+A basic decision tree that uses the iterative dichotomiser 3 algorithm,
+	and entropy.
+
+Notes:
+	The variable examples if frequently referenced throughout this file.
+	Examples should be a set of the following form for some string attr
+		{
+			(
+				[attr_1, attr_2, ...., attr_1], 
+				bool
+			)
+		}
+"""
+
+class Node:
+	"""
+	Node component of a basic tree.
+
+	Attributes:
+		label : str : this node's label
+		successors : [(Node, str)] : list of tuples of node, edge_label 
+			pairs
+	"""
+
+	def __init__(self, label):
+		"""
+		Construct this node.
+
+		Arguments:
+			label : str : this node's label
+		"""
+
+		self.label = label
+		self.successors = []
+
+	def add_successor(self, node, edge_label):
+		"""
+		Add a child node to this node.
+
+		Arguments:
+			node : Node : the Node object that will become a successor
+			edge_label : str : the label to apply to this edge
+		"""
+
+		self.successors.append((node, edge_label))
+
+class DecisionTree:
+	"""
+	Decision tree learner.
+
+	Attributes:
+		root : Node : the root of the decision tree
+	"""
+
+	def __init__(self, examples, target_attr, attrs):
+		"""
+		Fit the training data to learn the decision tree.
+
+		Arugments:
+			examples : {([str,], bool)} : set of examples to learn from
+			target_attr : str : attribute whose value is to be predicted
+			attrs : [str,] : list of attributes that may be tested by
+				the decision tree
+		"""
+
+		self.root = \
+			self.iterative_dichotomiser_3(examples, target_attr, attrs)
+
+	def iterative_dichotomiser_3(self, examples, target_attr, attrs):
+		"""
+		Learn the decision tree using the ID3 algorithm.
+
+		Arugments:
+			examples : {([str,], bool)} : set of examples to learn from
+			target_attr : str : attribute whose value is to be predicted
+			attrs : [str,] : list of attributes that may be tested by
+				the decision tree
+
+		Returns:
+			root : Node : a subcomponent of the tree learned
+		"""
+
+		pass
+
+	def information_gain(self, examples, attr):
+		"""
+		Compute the potentional information gain for a given attribute.
+
+		Arguments:
+			attr : str : the attribute to compute information gain for 
+				relative to some set of training examples
+			examples : {([str,], bool)} : set of examples to compute
+				the information gain for
+
+		Returns:
+			gain : float : the information gain computed
+		"""
+
+		pass
+
+	def entropy(self, examples):
+		"""
+		Compute the entropy of a set.
+
+		Arguments:
+			examples : {([str,], bool)} : set of examples to compute the
+				entropy for
+		"""
+
+		pass
+
+	def __str__(self):
+		"""
+		Represent this tree as a string.
+
+		This is an overload of the base python str() type cast. To use:
+			DT = DecisionTree()
+			s = str(DT)
+			print(s)
+
+		Returns:
+			s : str : the string representation of this decsision tree
+		"""
+
+		pass
+
+	def save(self, path):
+		"""
+		Save this decision tree to a file.
+
+		Arguments:
+			path : str : the .txt file to save this decision tree to
+		"""
+
+		pass
---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Run the program using this file
+
+	> python3 main.py input_path output_path
+
+	where 
+		input_path : the .csv input path to learn a decision tree from
+		output_path : the .txt output path to save the tree to
+"""
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
+
